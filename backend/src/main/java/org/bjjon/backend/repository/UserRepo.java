ae94d30838e0629a8bc705e5b6a6a44e0eba4499
bjoern <b.irion@wtnet.de>
2026-07-14 01:41:34 +0200

    Add user entity and repo

---
+++ backend/src/main/java/org/bjjon/backend/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package org.bjjon.backend.repository;
+
+import org.bjjon.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepo extends JpaRepository<User, UUID> {
+}
