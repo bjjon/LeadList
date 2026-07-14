c7fdfd6010b736c7fdf6d2943caa47cfa20ac2b6
bjoern <b.irion@wtnet.de>
2026-07-14 13:17:36 +0200

    Add JwtService tests

--- backend/src/main/java/org/bjjon/backend/security/JwtService.java
+++ backend/src/main/java/org/bjjon/backend/security/JwtService.java
@@ -1,7 +1,32 @@
 package org.bjjon.backend.security;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
 
 @Component
 public class JwtService {
+
+    private final SecretKey secretKey;
+    private final long expiration;
+
+    public JwtService(@Value("${jwt.secret}") String secretKey, @Value("${jwt.expiration}") long expiration) {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+        this.expiration = expiration;
+    }
+
+    public String generateToken(String email) {
+        return null;
+    }
+
+    public String extractEmail(String token) {
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        return false;
+    }
 }
