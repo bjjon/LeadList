d326db18520fb8d976e1ab7f4b0d6cbf873aa145
bjoern <b.irion@wtnet.de>
2026-07-14 13:58:21 +0200

    Implement Methods and run tests

--- backend/src/main/java/org/bjjon/backend/security/JwtService.java
+++ backend/src/main/java/org/bjjon/backend/security/JwtService.java
@@ -1,11 +1,14 @@
 package org.bjjon.backend.security;
 
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Component
 public class JwtService {
@@ -13,20 +16,38 @@ public class JwtService {
     private final SecretKey secretKey;
     private final long expiration;
 
-    public JwtService(@Value("${jwt.secret}") String secretKey, @Value("${jwt.expiration}") long expiration) {
-        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    public JwtService(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration}") long expiration) {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
         this.expiration = expiration;
     }
 
     public String generateToken(String email) {
-        return null;
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+                .issuedAt(now)
+                .subject(email)
+                .expiration(expirationDate)
+                .signWith(secretKey, Jwts.SIG.HS256)
+                .compact();
     }
 
     public String extractEmail(String token) {
-        return null;
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
     }
 
     public boolean validateToken(String token) {
-        return false;
+        try {
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException _) {
+            return false;
+        }
     }
 }
