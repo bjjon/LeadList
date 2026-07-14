98ebe30386f77f0eb6987304293f6ae4d9c6b3e8
bjoern <b.irion@wtnet.de>
2026-07-14 18:59:22 +0200

    Implement security config test

--- backend/src/test/java/org/bjjon/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/bjjon/backend/BackendApplicationTests.java
@@ -3,9 +3,11 @@ package org.bjjon.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @Import(TestcontainersConfiguration.class)
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
     @Test
